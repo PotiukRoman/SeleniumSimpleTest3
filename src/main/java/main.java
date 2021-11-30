96f057911e897ede5c9eb3593c92e15cc4320d37
TeamCodeCreator <potyuk@ukr.net>
2021-11-30 15:13:16 +0200

    getText method ptactice

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -14,15 +14,28 @@ public class main {
         System.setProperty("webdriver.chrome.driver","c:/Users/38099/Downloads/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
-        //    WebElement element=(new WebDriverWait(driver, Duration.ofSeconds(2))
-        //            .until(ExpectedConditions.presenceOfElementLocated(By.id("123"))));
-
         driver.get("https://www.avito.ru");
+
+//очікування (явне і неявне)
+/*
+            WebElement element=(new WebDriverWait(driver, Duration.ofSeconds(2))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.id("123"))));
+*/
+
+//властивості атрибутів тегів  різних елементів
+/*
         WebElement element = driver.findElement(By.xpath("(//a[text()='Личные вещи'])[1]"));
-        // String par=element.getAttribute("offsetWidth");
+     // String par=element.getAttribute("offsetWidth");
         String par=element.getAttribute("href");
         System.out.println(par);
+*/
+
+//Отимання тексту getText()
+
+        WebElement element = driver.findElement(By.cssSelector(".price-text-_WjC0.text-text-LurtD.text-size-s-BxGpL"));
+        String par=element.getText();
+        System.out.println(par);
+
     }
 }
 // c:/Users/38099/Downloads/chromedriver.exe
